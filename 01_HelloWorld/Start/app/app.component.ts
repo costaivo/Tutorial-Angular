f013761b18129a2e6ce5243f8d269b9672d5bc19
Ivo Costa <ivo@CreativeCapsule.local>
2016-05-05 13:34:51 +0530

    updated helloworld application

--- 01_HelloWorld/Start/app/app.component.ts
+++ 01_HelloWorld/Start/app/app.component.ts
@@ -1,11 +1,7 @@
-import {Component}from 'angular2/core';
+//import component from angular core
 
-@Component({
-    selector:'adm-app',
-    template:`<div><h1>{{pageTitle}}</h1>
-    </div>`
-})
 
-export class AppComponent{
-    pageTitle:string="Angular2: Hello World;
-}
+//define the component 
+
+
+//create class for the component
--- 01_HelloWorld/Start/app/main.ts
+++ 01_HelloWorld/Start/app/main.ts
@@ -1,6 +1,7 @@
-import { bootstrap } from 'angular2/platform/browser';
+// import bootstrap 
 
-// Our main component
-import { AppComponent } from './app.component';
 
-bootstrap(AppComponent);
+// import our component
+
+
+//bootstrap the component
