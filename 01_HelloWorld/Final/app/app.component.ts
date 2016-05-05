119f584182ea51f2f8a51e9beb7a124fab68e578
Ivo Costa <ivo@CreativeCapsule.local>
2016-05-05 14:14:58 +0530

    Implemented Hello World application

---
+++ 01_HelloWorld/Final/app/app.component.ts
@@ -0,0 +1,15 @@
+//import component from angular core
+import {Component} from 'angular2/core'
+
+//define the component 
+@Component({
+    selector:'my-app',
+    template:`<div><h1>Hello World</h1>
+    </div>`
+})
+
+
+//create class for the component
+export class AppComponent{
+    
+}
---
+++ 01_HelloWorld/Final/app/main.ts
@@ -0,0 +1,8 @@
+// import bootstrap 
+import {bootstrap} from 'angular2/platform/browser';
+
+// import our component
+import {AppComponent} from './app.component';
+
+//bootstrap the component
+bootstrap(AppComponent);
