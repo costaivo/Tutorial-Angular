854eaff023288db6578e707887ef4186af68a67d
Ivo Costa <ivo@CreativeCapsule.local>
2016-05-06 14:26:30 +0530

    implemented grid loading

--- APM - WIP/app/products/product-list.component.ts
+++ APM - WIP/app/products/product-list.component.ts
@@ -1,9 +1,37 @@
 import {Component} from 'angular2/core';
 
 @Component({
-    selector:'pm-products',
-    templateUrl:'app/products/product-list.component.html'
+    selector: 'pm-products',
+    templateUrl: 'app/products/product-list.component.html'
 })
-export class ProductListComponent{
-    
+export class ProductListComponent {
+    pageTitle: string = "Product List";
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage:boolean=false;
+    listFilter:string="cart";
+    products: any[] = [
+        {
+            "productId": 1,
+            "productName": "Leaf Rake",
+            "productCode": "GDN-0011",
+            "releaseDate": "March 19, 2016",
+            "description": "Leaf rake with 48-inch wooden handle.",
+            "price": 19.95,
+            "starRating": 3.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+        },
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
+        }];
+        toggleImage():void{
+            this.showImage = !this.showImage;
+        }
 }
