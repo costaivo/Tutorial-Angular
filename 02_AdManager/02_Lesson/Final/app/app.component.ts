f7998632092fbec29a43d40e5f46226ad42c1a68
Ivo Costa <ivo@CreativeCapsule.local>
2016-06-07 18:41:40 +0530

    updated lesson 02

--- 02_AdManager/02_Lesson/Final/app/app.component.ts
+++ 02_AdManager/02_Lesson/Final/app/app.component.ts
@@ -1,10 +1,13 @@
 import  {Component} from 'angular2/core';
+import {PublicationListComponent} from './publications/publication-list.component';
 
 @Component({
     selector:'am-app',
     template:`<div >
       <h1> {{pageTitle}} : Advertisment Manager</h1>
-       </div>`
+      <am-publicationList></am-publicationList>
+       </div>`,
+       directives:[PublicationListComponent]
 })
 export class AppComponent{
     pageTitle:string = 'Digital Ads';
---
+++ 02_AdManager/02_Lesson/Final/app/publications/publication-list.component.ts
@@ -0,0 +1,11 @@
+import {Component} from 'angular2/core'
+
+@Component({
+    selector:'am-publicationList',
+    templateUrl:'app/publications/publication-list.component.html'
+})
+
+export class PublicationListComponent
+{
+    pageTitle:string="Publications";
+}
