4d44eed22a5ffbe5a64dcf112729041aa12d10e4
Ivo Costa <ivo@CreativeCapsule.local>
2016-09-26 10:18:09 +0530

    implemented Master/detail form

--- Playground/app/app.component.ts
+++ Playground/app/app.component.ts
@@ -1,20 +1,26 @@
 import {Component} from '@angular/core';
 
-export class Hero{
-    id:number;
-    name:string;
-}
+
+
+const HEROES: Hero[] = [
+  { id: 11, name: 'Mr. Nice' },
+  { id: 12, name: 'Narco' },
+  { id: 13, name: 'Bombasto' },
+  { id: 14, name: 'Celeritas' },
+  { id: 15, name: 'Magneta' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr IQ' },
+  { id: 19, name: 'Magma' },
+  { id: 20, name: 'Tornado' }
+];
+
 
 @Component({
     selector:'my-app',
-    template:`<h1>{{title}}</h1>
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id:</label>{{hero.id}}</div>
-    <div>
-        <label>name:</label>
-        <input [(ngModel)]="hero.name" placeholder="name">
-    </div>
-    `
+    templateUrl:'./app/app.component.html',
+    styleUrls:['./app/app.component.css']
+    
 
 })
 
@@ -25,10 +31,12 @@ export class Hero{
 */
 export class AppComponent{
 
-    hero:Hero ={
-        id:1,
-        name:'Windstorm'
-    };
+   heroes = HEROES;
+   selectedHero:Hero;
+
     title='Tour of Heroes';
   
+    onSelect(hero:Hero):void{
+        this.selectedHero=hero;
+    }
 }
