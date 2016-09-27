e7c2993ff9e2feadfcc5b706ac3f7b30a7f6f6d3
Ivo Costa <ivo@CreativeCapsule.local>
2016-09-27 12:53:04 +0530

    Implemented Services

--- Playground/app/app.component.ts
+++ Playground/app/app.component.ts
@@ -1,27 +1,16 @@
-import {Component} from '@angular/core';
+import {Component,OnInit} from '@angular/core';
 
 
 import {Hero} from './hero';
+import {HeroService} from './hero.service';
 
-const HEROES: Hero[] = [
-  { id: 11, name: 'Mr. Nice' },
-  { id: 12, name: 'Narco' },
-  { id: 13, name: 'Bombasto' },
-  { id: 14, name: 'Celeritas' },
-  { id: 15, name: 'Magneta' },
-  { id: 16, name: 'RubberMan' },
-  { id: 17, name: 'Dynama' },
-  { id: 18, name: 'Dr IQ' },
-  { id: 19, name: 'Magma' },
-  { id: 20, name: 'Tornado' }
-];
 
 
 @Component({
     selector:'my-app',
     templateUrl:'./app/app.component.html',
-    styleUrls:['./app/app.component.css']
-    
+    styleUrls:['./app/app.component.css'],
+    providers:[HeroService]
 
 })
 
@@ -30,14 +19,24 @@ const HEROES: Hero[] = [
     {{}}  -- Interpolation : One-way data binding. 
     template:'' --single quote, back-ticks for multiline. 
 */
-export class AppComponent{
+export class AppComponent implements OnInit{
 
-   heroes = HEROES;
-   selectedHero:Hero;
+constructor(private heroservice:HeroService){
 
+}
+   
+    selectedHero:Hero;
+    heroes: Hero [];
     title='Tour of Heroes';
   
     onSelect(hero:Hero):void{
         this.selectedHero=hero;
+    }
+
+    ngOnInit():void{
+        this.getHeroes();
+    }
+    getHeroes():void{
+        this.heroservice.getHeroesSlowly().then(heroes=> this.heroes = heroes);
     }
 }
--- Playground/app/hero-detail.component.ts
+++ Playground/app/hero-detail.component.ts
@@ -5,7 +5,6 @@ import {Hero} from './hero';
 @Component({
   selector: 'my-hero-detail',
   templateUrl:'./app/hero-detail.component.html'
-  //template:'Hello BABA'
 })
 export class HeroDetailComponent {
     @Input()
