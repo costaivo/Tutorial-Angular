295c614849a617a965fffac1d0eee777129842a2
Ivo Costa <ivo@CreativeCapsule.local>
2016-09-29 23:38:43 +0530

    completed quickstart

--- Playground/app/app.module.ts
+++ Playground/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 import {FormsModule} from '@angular/forms';
 import {routing} from './app.routing';
 import {HttpModule} from '@angular/http';
+import './rxjs-extensions';
 
 // Imports for loading & Configuring the in-memory web api
 import {InMemoryWebApiModule} from 'angular2-in-memory-web-api';
@@ -13,6 +14,7 @@ import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 import {HeroService} from './hero.service';
 import {DashboardComponent} from './dashboard.component';
+import {HeroSearchComponent} from './hero-search.component';
 
 @NgModule({
     imports:[
@@ -26,7 +28,8 @@ import {DashboardComponent} from './dashboard.component';
         AppComponent,
         HeroDetailComponent,
         HeroesComponent,
-        DashboardComponent
+        DashboardComponent,
+        HeroSearchComponent
         ],
     providers:[HeroService],
     bootstrap:[AppComponent],
