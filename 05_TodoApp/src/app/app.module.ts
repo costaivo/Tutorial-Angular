b5182819e4147c0921462c51d6e721cec2c9fb67
Ivo Costa <ivo@CreativeCapsule.local>
2016-10-02 12:45:39 +0530

    Todo Application Completed

--- 05_TodoApp/src/app/app.module.ts
+++ 05_TodoApp/src/app/app.module.ts
@@ -4,17 +4,23 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import {TodoService} from './todo.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, TodosComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [TodoService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(){
+    console.log("App Module Initialized");
+  }
+}
---
+++ 05_TodoApp/src/app/todos/todos.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { TodosComponent } from './todos.component';
+
+describe('Component: Todos', () => {
+  it('should create an instance', () => {
+    let component = new TodosComponent();
+    expect(component).toBeTruthy();
+  });
+});
