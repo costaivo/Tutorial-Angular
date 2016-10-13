34019cc741b218111d39ee6cd39f60750213aab4
Ivo Costa <ivo@CreativeCapsule.local>
2016-10-13 16:54:42 +0530

    Added lesson 04 structure

---
+++ 02_AdManager/04_Lesson/Final/readme.md
@@ -0,0 +1,1 @@
+# [Lesson 03 -- Component & DataBinding](https://github.com/costaivo/AngularJs2-AdManager/tree/Dev/02_AdManager/03_Lesson/Start)
---
+++ 02_AdManager/04_Lesson/Start/app/app.component.ts
@@ -0,0 +1,27 @@
+//import Component from angular core
+import {Component} from '@angular/core';
+
+//define the metadata for the component using decorator
+ @Component({
+     selector:'am-app',
+     templateUrl:'app/app.component.html',
+     styleUrls:['app/app.component.css']
+})
+
+
+//create class for the component
+export class AppComponent{
+    pageTitle:string = "Digital Ads : Ad Manager";
+    
+    fontColor="red";
+    publication={
+        "ID": "c7bd9a71-a1a4-4d39-ab91-be966512bd0e",
+        "IsActiveRecord": true,
+        "Name": "Herald",
+        "TypexCD": "Local",
+        "LanguagexCD": "English",
+        "CommissionRateForAdvertisments": 0.15,
+        "CommisionRateForClassifieds": 0.059
+    };
+
+}
