89d2aed3b45ebcd1f113529348ee1b5d121ba4b0
Ivo Costa <ivo@CreativeCapsule.local>
2016-10-22 17:28:35 +0530

    fixed warnings reported by tslint

--- 04_GithubApp/README.md
+++ 04_GithubApp/README.md
@@ -1,3 +1,4 @@
 # GitHub Application
 
 1. Register GitHub application on github website
+1. Create a component github.component
--- 04_GithubApp/src/app/github/github.service.ts
+++ 04_GithubApp/src/app/github/github.service.ts
@@ -1,14 +1,15 @@
 import {Injectable} from '@angular/core';
-import {Http,Headers} from '@angular/http';
+import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class GithubService{
-    private username='costaivo';
-    private client_id='';
-    private client_secret='';
 
-    constructor(private _http:Http){
-        console.log('github service init');
+    private username = 'costaivo';
+    private client_id = '9faec800821a3d0a58b0';
+    private client_secret = '373731bb8a1cefe47db36c1e372f06bed99a6e83';
+
+    constructor(private _http: Http) {
+        console.log('Github service Init...');
     }
 }
