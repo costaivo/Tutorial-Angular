9a0277b6b8b9c07519af3caf4a616dd5641cbee2
Ivo Costa <ivo@CreativeCapsule.local>
2016-10-25 00:53:03 +0530

    completed github application

--- 04_GithubApp/src/app/github/github.service.ts
+++ 04_GithubApp/src/app/github/github.service.ts
@@ -8,18 +8,19 @@ export class GithubService {
   private username = 'costaivo';
   private client_id = '9faec800821a3d0a58b0';
   private client_secret = '373731bb8a1cefe47db36c1e372f06bed99a6e83';
-
+  private urlPostfix:string = "";
   constructor(private _http: Http) {
     console.log('Github service Init...');
+  this.urlPostfix = `?client_id=${this.client_id}&client_secret=${this.client_secret}`;
   }
 
   getUser() {
 
-    return this._http.get('https://api.github.com/users/' + this.username)
+    return this._http.get('https://api.github.com/users/' + this.username + this.urlPostfix)
       .map(res => res.json());
   }
   getRepos() {
-    return this._http.get('https://api.github.com/users/' + this.username + '/repos')
+    return this._http.get('https://api.github.com/users/' + this.username + '/repos' + this.urlPostfix)
       .map(res => res.json());
   }
   updateUsername(username: string) {
