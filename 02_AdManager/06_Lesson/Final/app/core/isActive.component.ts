d500652c60b1d14f7bf044b52eefea17e1083aa8
Ivo Costa <ivo.costa@creativecapsule.com>
2016-10-29 19:26:46 +0530

    Renamed shared folder to core

--- 02_AdManager/06_Lesson/Final/app/core/isActive.component.ts
+++ 02_AdManager/06_Lesson/Final/app/core/isActive.component.ts
@@ -1,7 +1,7 @@
 import {Component,Input} from '@angular/core';
 
 @Component({
-    selector:'am-activeRecord',
+    selector:'ct-activeRecord',
     templateUrl:'app/shared/isActive.component.html'
 })
 
