0083d21ad95585de9aacf3c51df11ee88c0cab37
Ivo Costa <ivo.costa@creativecapsule.com>
2016-10-30 14:29:04 +0530

    formatted cards

--- 02_AdManager/07_Lesson/Final/app/dashboard/dashboard.component.ts
+++ 02_AdManager/07_Lesson/Final/app/dashboard/dashboard.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-    moduleId:module.id,
+    moduleId: module.id,
     selector: 'am-dashboard',
     templateUrl: 'dasboard.component.html',
-    styleUrls:['dasboard.component.css']
+    styleUrls: ['dasboard.component.css']
 })
-export class DasboardComponent  {
+export class DasboardComponent {
 
+    publicationCount: number = 10;
+    clientCount: number = 10;
 }
