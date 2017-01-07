b81a463e1a8b49e1448cc8399fa04df635278748
CREATIVECAPSULE\ivo <costa_ivo@yahoo.com>
2017-01-07 16:23:16 +0530

    fixed bugs related to missing files references

--- Playground/Forms/src/app/app.module.ts
+++ Playground/Forms/src/app/app.module.ts
@@ -7,23 +7,17 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpModule } from '@angular/http';
 
-import { AboutComponent } from './about/about.component';
+
 import { HomeComponent } from './home/home.component';
-import { RepoBrowserComponent } from './github/repo-browser/repo-browser.component';
-import { RepoListComponent } from './github/repo-list/repo-list.component';
-import { RepoDetailComponent } from './github/repo-detail/repo-detail.component';
+
+
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { ContactComponent } from './contact/contact.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    AboutComponent,
-    RepoBrowserComponent,
-    RepoListComponent,
-    RepoDetailComponent,
     HomeComponent,
-    ContactComponent
   ],
   imports: [
     BrowserModule,
