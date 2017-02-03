fd06eceeaf80ab390ef2602a59bab93665266c99
CREATIVECAPSULE\ivo <costa_ivo@yahoo.com>
2017-02-03 22:31:00 +0530

    add code to fetch client data using http service

--- 02_AdManager/08_Lesson/Final/app/clients/client.service.ts
+++ 02_AdManager/08_Lesson/Final/app/clients/client.service.ts
@@ -1,52 +1,52 @@
 import { Injectable } from '@angular/core';
+
+//required for http service
+import {Http,Response} from '@angular/http';
+
+//required for return type for http requests
+import {Observable} from 'rxjs/Observable';
+
+//Load the library into memory, but does not import any modules
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
+
 import { IClient } from './client';
 
 @Injectable()
 export class ClientService {
 
-    constructor() {
+    private _clientUrl ='api/clients/clients.json';
+
+    //Inject the http service
+    constructor(private _http:Http) {
 
     }
    
-	getTotalCount():number{
-		return this.getAll().length;
-    }
-    getClient(id:string):IClient{
-		return this.getAll().find(x=>x.ID==id);
+	getTotalCount():Observable<number>{
+		return this._http.get(this._clientUrl)
+        .map((Response:Response)=>(<IClient[]>Response.json()).length)
+        .do(data=>console.log('Count : '+JSON.stringify(data)))
+        .catch(this.handleError);
     }
     
-    getAll(): IClient [] {
-        return [{
-            "ID": "1E916F75-E36F-4842-82B2-01647E75C79D",
-            "IsActiveRecord": true,
-            "FirstName": "Gaurav",
-            "MiddleName": "",
-            "LastName": "Borkar",
-            "Address": "Margao",
-            "MobileNumber": "9765392024",
-            "TypexCD": "Sub Agent"
-        },
-        {
-            "ID": "0A4AB922-962B-47CC-8B82-02E6F0A495F4",
-            "IsActiveRecord": true,
-            "FirstName": "Zohaib",
-            "MiddleName": "",
-            "LastName": "Mulla",
-            "Address": "Nuvem",
-            "MobileNumber": "9822158470",
-            "TypexCD": "Regular Client"
-        },
-        {
-            "ID": "C57AA67E-7874-4708-8665-0DDC8B0FAA0F",
-            "IsActiveRecord": true,
-            "FirstName": "Nathan",
-            "MiddleName": "",
-            "LastName": "Costa",
-            "Address": "Margao",
-            "MobileNumber": "9823163587",
-            "TypexCD": "Regular Client"
-        }
-        ];
+    getClient(id:string):Observable<IClient>{
+		return this._http.get(this._clientUrl)
+        .map((response:Response)=> (<IClient[]>response.json()).find(x=>x.ID==id))
+        .do(data => console.log('client :'+JSON.stringify(data)))
+        .catch(this.handleError);
+    }
+    
+    getAll(): Observable<IClient []> {
+        return this._http.get(this._clientUrl)
+        .map((response:Response)=> <IClient[]>response.json())
+        .do(data=> console.log('All Clients :'+JSON.stringify(data)))
+        .catch(this.handleError);
+    }
+
+    private handleError(error:Response){
+        console.error(error);
+        return Observable.throw(error.json().error || 'Server error');
     }
 }
 
