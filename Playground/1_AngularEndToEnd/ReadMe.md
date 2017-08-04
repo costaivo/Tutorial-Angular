eef5be53f88f2a7bee52525870b2414aac8603c5
Costa Ivo <costa_Ivo@yahoo.com>
2017-08-04 23:50:06 +0530

    Deleted Playground

--- Playground/1_AngularEndToEnd/ReadMe.md
+++
@@ -1,3 +0,0 @@
-# Angular 2 End to End
-
-https://app.pluralsight.com/library/courses/angular-2-end-to-end/table-of-contents
--- Playground/note-keeper/src/main.ts
+++
@@ -1,7 +0,0 @@
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-
-import {AppModule} from './app';
-
-
-
-platformBrowserDynamic().bootstrapModule(AppModule);
